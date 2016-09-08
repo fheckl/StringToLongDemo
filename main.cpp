5b73eb9455e69b6a6f0dc2eb9738d990865869a9
Florian Heckl <florian@florian-heckl.de>
2016-09-08 10:47:23 +0200

    Completely in English  now

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 /**
- * Das ist die Methode, wie sie bisher bei uns implementiert ist.
- * Problem dabei: Parsing-Fehler - insbesondere wenn der zu parsende Wert zu groß ist - werden nicht erkannt. 
- * Der C++-Standard (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3690.pdf) 
- * sieht für diesen Fall vor (im Beispiel auf Seite 1053), dass in l der maximale oder minimale (je nach Ausgangswert) Wert
- * des definierten Typs steht.
- * Microsoft weicht davon ab und belässt entweder den von uns vorher gesetzten Wert in der Variablen oder aber sie setzen ihn
- * explizit auf 0 (ich habe es nicht genauer geprüft).
- */ 
+ * \brief The original implementation as we currently have it. The problem here is that parsing errors - e.g. when the value does not fit
+ * into the desired type - are not detected.
+ * The C++-11 standard (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3690.pdf) falls back to storing the min or max value for the type
+ * (depending on the parsed value) as can be seen in the example on page 1053.
+ * Microsoft does not seem to follow the standard there since they leave the value untouched instead.
+ * \param s the string to be parsed
+ * \return the parsed value as determined by istringstream's >> operator
+ */
 long stringToLong(std::string s)
 {
     std::istringstream is(s);
@@ -23,26 +23,30 @@ long stringToLong(std::string s)
 }
 
 /**
- * In dieser ersten verbesserten Version wird nach dem Lesen aus dem Stream überprüft, ob es dabei zu einem Fehler kam und - falls
- * das der Fall ist - wird der Fallback-Wert zurückgegeben.
- */ 
+ * \brief In this first improved version we test for errors after the stream operation. In case of errors the fallback value is returned.
+ * \param s the string to be parsed
+ * \param fallBack the value to be used in case of parsing errors
+ * \return the parsed value as determined by istringstream's >> operator if it succeeds, the fallback value otherwise
+ */
 long stringToLongImproved1(string s, long fallBack = 60L)
 {
     istringstream is(s);
     long l = 0;
     is >> l;
 
-    // Nachprüfen: Ging die >>-Operation denn gut?
+    // Check: Was the >> operation successful?
     if (is.fail()) return fallBack;
 
     return l;
 }
 
 /**
- * In dieser zweiten verbesserten Version wird die in neueren C++-Versionen definierte Methode stol zum Paresen verwendet.
- * Diese wirft eine Exception, wenn der gelesene Wert nicht in der Variablen (bzw. deren Typ) untergebracht werden kann, woraufhin
- * wir wieder den Fallback-Wert zurückgeben.
- */ 
+ * \brief The second improved version uses the method stol as present in more recent C++ standard libraries. stol throws an exception in case
+ * of parsing errors so we can use this to return our fallback value.
+ * \param s the string to be parsed
+ * \param fallBack the value to be used in case of parsing errors
+ * \return the parsed value as determined by stol if it succeeds, the fallback value otherwise
+ */
 long stringToLongImproved2(string s, long fallBack = 60L)
 {
     try {
@@ -56,22 +60,21 @@ long stringToLongImproved2(string s, long fallBack = 60L)
 }
 
 int main() {
-    // Stream >>-Operator siehe: C++11-Standard (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3690.pdf) Seite 1053
-    // Dieser Wert stand z. B. so in der Datenbank und wurde mit stringToLong verarbeitet
+    // This is our test value which is too large to fit into a long variable
     string s("31536000000");
 
-    // 1. Versuch: bisherige Methode
-    // MSVC++ liefert hier 0 als Ergebnis, während andere moderne Compiler - dem Standard folgend - je nach Situation den max. oder min. Wert von long liefern
-    auto ergebnis = stringToLong(s);
-    cout << "Ergebnis (eigen): " << ergebnis << endl;
+    // 1st try using our current function
+    // MSVC++ returns 0 as the result while other modern compilers (g++, clang) adhere to standard and return the max long value
+    auto result = stringToLong(s);
+    cout << "Result (current function): " << result << endl;
 
-    // 2. Versuch: bisherige Methode mit Fehlerbehandlung
-    ergebnis = stringToLongImproved1(s);
-    cout << "Ergebnis (eigen mit Fehlerbehandlung): " << ergebnis << endl;
+    // 2nd try using the first improved version
+    result = stringToLongImproved1(s);
+    cout << "Result (improved v1): " << result << endl;
 
-    // 3. Versuch: stol mit Fallback
-    ergebnis = stringToLongImproved2(s);
-    cout << "Ergebnis (stol mit Fallback): " << ergebnis << endl;
+    // 3rd try using the second improved version
+    result = stringToLongImproved2(s);
+    cout << "Result (improved v2): " << result << endl;
 
     getchar();
 
