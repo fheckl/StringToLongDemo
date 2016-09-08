5d19bf6724bc19313a8ec88ecd0c5bf4aea9d007
Florian Heckl <florian@florian-heckl.de>
2016-09-08 09:59:23 +0200

    Kommentare ausgebaut

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+/**
+ * Das ist die Methode, wie sie bisher bei uns implementiert ist.
+ * Problem dabei: Parsing-Fehler - insbesondere wenn der zu parsende Wert zu groß ist - werden nicht erkannt. 
+ * Der C++-Standard (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3690.pdf) 
+ * sieht für diesen Fall vor (im Beispiel auf Seite 1053), dass in l der maximale oder minimale (je nach Ausgangswert) Wert
+ * des definierten Typs steht.
+ * Microsoft weicht davon ab und belässt entweder den von uns vorher gesetzten Wert in der Variablen oder aber sie setzen ihn
+ * explizit auf 0 (ich habe es nicht genauer geprüft).
+ */ 
 long stringToLong(std::string s)
 {
     std::istringstream is(s);
@@ -13,6 +22,10 @@ long stringToLong(std::string s)
     return l;
 }
 
+/**
+ * In dieser ersten verbesserten Version wird nach dem Lesen aus dem Stream überprüft, ob es dabei zu einem Fehler kam und - falls
+ * das der Fall ist - wird der Fallback-Wert zurückgegeben.
+ */ 
 long stringToLongImproved1(string s, long fallBack = 60L)
 {
     istringstream is(s);
@@ -25,6 +38,11 @@ long stringToLongImproved1(string s, long fallBack = 60L)
     return l;
 }
 
+/**
+ * In dieser zweiten verbesserten Version wird die in neueren C++-Versionen definierte Methode stol zum Paresen verwendet.
+ * Diese wirft eine Exception, wenn der gelesene Wert nicht in der Variablen (bzw. deren Typ) untergebracht werden kann, woraufhin
+ * wir wieder den Fallback-Wert zurückgeben.
+ */ 
 long stringToLongImproved2(string s, long fallBack = 60L)
 {
     try {
